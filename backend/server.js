84d42e7ee78cecc8c6bc9b85aa8c4903614a01a0
yamasashii <163961741+yamasashii@users.noreply.github.com>
2026-03-11 23:33:20 +0700

    fix db connection

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
